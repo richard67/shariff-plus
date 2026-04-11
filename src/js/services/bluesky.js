ceb3d8e8823d58d0ec624a8cdf4d054092a4d0d6
Richard Fath <richard.fath@t-online.de>
2026-04-11 16:27:47 +0200

    Fix typo from previous commit

--- src/js/services/bluesky.js
+++ src/js/services/bluesky.js
@@ -27,7 +27,7 @@ export default function data(shariff) {
   var url = shariff.getURL()
   // From Bluesky documentation (Decembe 2024):
   // The post length limit on Bluesky is 300 characters.
-  var text = abbreviateText(title, 299 - url.length));
+  var text = abbreviateText(title, 299 - url.length);
   if (text.length > 0) {
     text += ' ' + url;
   } else {
